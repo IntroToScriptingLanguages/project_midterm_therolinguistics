c5074b0247905f532f2c1236961da3f4d176290a
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-10-17 13:25:20 -0500

    frontend stuff working

--- squirrel.js
+++ squirrel.js
@@ -1,24 +1,25 @@
 jQuery(document).ready(function($){
 
  var output = getQueryStringArray()["input"];
- var phonetics = $("#phonetics");
- phonetics.html(encodeURIComponent(output))
  $("#output").html(output);
-
 });
-
-var pronounce = [""]
 
 function getQueryStringArray()
 {
   var queries = {};
+  var newString;
   $.each(document.location.search.substr(1).split('&'), function(index, value) { /* Gets query string of URL, splits it along each of the indices (&) and stores it into an array, then parses it!*/
     var i = value.split('='); /* Takes the assignment of each variable and splits it between key and element, returning array i */
-    var newString = i[1].toString().replace("/+/g", "");
-    newString = newString.replace("/(%0D%0A)/g", "");
+    newString = i[1].toString().replace("+", " ");
+    newString = newString.replace("%0D%0A", "\n");
     queries[i[0].toString()] = newString; /* Assigns the element to its corresponding key in "queries" */
   });
   return queries;
+}
+
+function isSquirrel()
+{
+  return $("foreign").css("font-family") === "Arial";
 }
 
 function toEnglish() {
