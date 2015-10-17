fce07721e97322ddd3649ae536a56357b1dc4064
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-10-17 15:54:41 -0500

    IT FINALLY WORKS!!!

--- squirrel.js
+++ squirrel.js
@@ -1,11 +1,10 @@
 jQuery(document).ready(function($){
 
  var output_array = getQueryStringArray()
- if (output_array.length > 0)
+ if (document.location.search.length > 0)
  {
-   console.log("It actually worked...");
    var output = output_array["input"];
-   if (output_array["lang"] = "sq") //Converting from English to Squirrel
+   if (output_array["lang"] === "eng") //Converting from English to Squirrel
    {
      toEnglish();
    }
@@ -18,15 +17,6 @@ jQuery(document).ready(function($){
 
 function submit()
 {
-  var form = $("#lang");
-  if (isSquirrel())
-  {
-    form.val("sq"); //Convert from English to Squirrel
-  }
-  else {
-    form.val("eng"); //Convert from Squirrel to English
-  }
-  console.log(form.val());
   $("#input_form").submit();
 }
 
@@ -38,27 +28,44 @@ function getQueryStringArray()
   {
     $.each(document.location.search.substr(1).split('&'), function(index, value) { /* Gets query string of URL, splits it along each of the indices (&) and stores it into an array, then parses it!*/
       var i = value.split('='); /* Takes the assignment of each variable and splits it between key and element, returning array i */
-      newString = i[1].toString().replace("+", " ");
-      newString = newString.replace("%0D%0A", "\n");
+      newString = i[1].toString().replace(/\+/g, " ");
+      newString = newString.replace(/\%0D\%0A/g, "\n");
       queries[i[0].toString()] = newString; /* Assigns the element to its corresponding key in "queries" */
     });
   }
-  console.log(queries);
   return queries;
+}
+
+function setLanguage()
+{
+  if ($("#lang").options[$("#lang").selectedIndex].value = "sq") //Converting from English to Squirrel
+  {
+    toEnglish();
+  }
+  else { //Convert from Squirrel to English (default)
+    toSquirrel();
+  }
+}
+
+function isEnglish()
+{
+  return ($("native").css("font-family") === "Arial");
 }
 
 function isSquirrel()
 {
-  return $("foreign").css("font-family") === "Arial";
+  return ($("foreign").css("font-family") === "Arial");
 }
 
 function toEnglish() {
   $(".native").css("font-family", "Arial");
   $(".foreign").css("font-family", "Wingdings");
+  $("#idioma").val("eng");
 }
 
 function toSquirrel()
 {
   $(".native").css("font-family", "Wingdings");
   $(".foreign").css("font-family", "Arial");
+  $("#idioma").val("sq");
 }
