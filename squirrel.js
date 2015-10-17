a2b0eadcd7fa980ada82a87d036d60a202d090cb
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-10-17 11:58:48 -0500

    things are working out

--- squirrel.js
+++ squirrel.js
@@ -1,6 +1,29 @@
 jQuery(document).ready(function($){
 
- var output = $("#translate").input;
- $("#output").html(input);
+ var output = getQueryStringArray()["input"];
+ $("#output").html(output);
 
 });
+
+function getQueryStringArray()
+{
+  var queries = {};
+  $.each(document.location.search.substr(1).split('&'), function(index, value) { /* Gets query string of URL, splits it along each of the indices (&) and stores it into an array, then parses it!*/
+    var i = value.split('='); /* Takes the assignment of each variable and splits it between key and element, returning array i */
+    var newString = i[1].toString().replace("/+/g", "");
+    newString = newString.replace("/(%0D%0A)/g", "");
+    queries[i[0].toString()] = newString; /* Assigns the element to its corresponding key in "queries" */
+  })
+  return queries;
+}
+
+function toEnglish() {
+  $(".native").css("font-family", "Arial");
+  $(".foreign").css("font-family", "Wingdings");
+}
+
+function toSquirrel()
+{
+  $(".native").css("font-family", "Wingdings");
+  $(".foreign").css("font-family", "Arial");
+}
